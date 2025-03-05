ff7627be2998c6dec24be84f6412abaa9d7adf0f
Jace <121627078+Artzkaizen@users.noreply.github.com>
2025-03-05 15:43:06 +0100

    add deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "artzkaizen.github.io",
-  base: "https://github.com/artzkaizen/zeit-guard",
   vite: {
     plugins: [tailwindcss()],
   },
