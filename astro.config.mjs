fad3a5e6cbfeef00b1f3eab513e280f54bda7b8e
Jace <121627078+Artzkaizen@users.noreply.github.com>
2025-03-05 15:47:07 +0100

    fix invalid url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "artzkaizen.github.io",
+  site: "https://artzkaizen.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
